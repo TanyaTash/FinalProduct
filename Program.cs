84725625b32336be5d3668c179f2f105226b0577
TanyaTash <tanya.tash0@gmail.com>
2023-12-29 23:09:39 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] source = { "единицы", "десятки", "сотни", "тысячи", "1", ":-)", "New York", "-2", "Windows" };
+void PrintArray(string[] m)
+{
+    for (int x = 0; x < m.Length; x++)
+        Console.Write($"\"{m[x]}\" ");
+    Console.WriteLine();
+}
+string[] copyStringsBelowOrEqual3InNewArray(string[] strArray)
+{
+    int k = 0;
+    for (int i = 0; i < strArray.Length; i++)
+        if (strArray[i].Length <= 3) k++;
+    string[] newArray = new string[k];
+
+    k = 0;
+    for (int i = 0; i < strArray.Length; i++)
+        if (strArray[i].Length <= 3) newArray[k++]=strArray[i];
+
+    return newArray;
+}
+
+Console.WriteLine("Программа создания нового массива и копирования в него строк длиной <=3");
+Console.WriteLine("Исходный массив строк:");
+PrintArray(source);
+
+string[] target = copyStringsBelowOrEqual3InNewArray(source);
+
+Console.WriteLine("Результирующий массив строк:");
+PrintArray(target);
+Console.WriteLine();
